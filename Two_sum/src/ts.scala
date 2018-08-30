5c9bf13377bbf29e85d246575135ab3438b165a5
Jonas Claesson <jonasc15@student.uia.no>
2018-08-30 14:59:45 +0200

    added two unfinished projects

---
+++ Two_sum/src/ts.scala
@@ -0,0 +1,21 @@
+
+
+object ts extends App {
+  val arr=List(1,5,3);
+  println(arr);
+  
+  def Find_Indices(target_Sum:Int) =
+  {
+    //var i=0;
+    for(i<-arr)
+    {
+      //if(arr(i))
+      println(i)
+      if(i+i==)
+    }
+  }
+  
+  val sum = 3;
+  
+  Find_Indices(sum);
+}
---
+++ next permutation/src/Solution.java
@@ -0,0 +1,38 @@
+/*
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+
+The replacement must be in-place and use only constant extra memory.
+
+Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+
+*/
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int[] arr = new int[3];
+		System.out.println(arr.length);
+		
+		for(int i = 0; i<arr.length; i++)
+		{
+			arr[i] = i+1;
+		}
+		
+		
+		
+	}
+
+	public void nextpermutation(int[] nums)
+	{
+		
+	}
+}
